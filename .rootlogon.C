e384748940abda261a17001e428902de59e759df
Sam <samcunliffe@users.noreply.github.com>
2013-06-04 16:57:03 +0100

    Made ganga.py PEP8 beautified.

--- .rootlogon.C
+++ .rootlogon.C
@@ -1,8 +1,11 @@
 {
-  cout << endl << "\033[1mYou've got Sam's .rootlogon.C\033[0m" << endl;
+
+  cout << "\033[1mYou've got Sam's .rootlogon.C\033[0m" << endl;
   cout << "ROOT version " << gROOT->GetVersion() << endl;
   
-  //gROOT->ProcessLine(".x ~/.lhcbStyle.C");
+  gROOT->ProcessLine(".x ~/.lhcbStyle.C");
+
+
 }
 
 
